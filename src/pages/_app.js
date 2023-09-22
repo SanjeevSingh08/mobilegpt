150e77c9c82aeb095a3ab7a980d03eaad5b5bb5c
Sanjeev Ranjan Singh <100428475+SanjeevSingh08@users.noreply.github.com>
2023-09-22 11:17:45 +0530

    new

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import Head from 'next/head'; // Import the Head component
 
 export default function App({ Component, pageProps }) {
-  return <div className="bg-gray-800"><Component {...pageProps} /></div>
+  return (
+    <>
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Add other meta tags and head elements here */}
+      </Head>
+      <div className="bg-gray-800">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
